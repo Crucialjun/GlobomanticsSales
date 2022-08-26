3a4e3ccecaa32efe1f09690e491b7eee5168bb83
Crucial Jun <nich.otieno@gmail.com>
2022-08-26 20:16:01 +0300

    build pdf

---
+++ app/src/main/java/com/psdemo/globomanticssales/FileUtils.kt
@@ -0,0 +1,90 @@
+package com.psdemo.globomanticssales
+
+import android.content.Context
+import android.graphics.Color
+import android.graphics.Paint
+import android.graphics.Paint.Align.*
+import android.graphics.pdf.PdfDocument
+import android.text.format.DateFormat
+import android.util.Log
+import com.psdemo.globomanticssales.data.Client
+import java.io.File
+import java.io.FileOutputStream
+import java.io.IOException
+import java.util.*
+
+
+    private val FILE_NAME = "proposal.pdf"
+    private fun Context.getFolder(id:Int) : File {
+        val directory = File("${this.filesDir}/$id")
+
+        if(!directory.exists()){
+            directory.mkdir()
+        }
+
+        return directory
+    }
+
+    fun Context.buildPdf(client : Client){
+        val directory = this.getFolder(client.id)
+        val inch = 72
+        val halfInch = inch /2
+        val inchFloat = inch.toFloat()
+        val halfInchFloat = halfInch.toFloat()
+
+        val document = PdfDocument()
+        val pageInfo = PdfDocument.PageInfo.Builder(inch * 8 + halfInch,inch * 11,1).create()
+        val page = document.startPage(pageInfo)
+
+        val canvas = page.canvas
+
+        val paint = Paint()
+        paint.color = Color.BLACK
+        paint.textAlign = RIGHT
+
+        val calendar = Calendar.getInstance()
+        val dateFormat = DateFormat.getDateFormat(this)
+        calendar.timeInMillis = client.date
+        canvas.drawText(dateFormat.format(calendar.time),inch * 7 + halfInchFloat,inchFloat,paint)
+
+        val logo = this.resources.getDrawable(R.drawable.logo,theme)
+        logo.setBounds(inch,inch,inch * 6 + halfInch,inch * 2)
+        logo.draw(canvas)
+
+        val sharedPreferences = this.getSharedPreferences(getString(R.string.profile_preference_key),Context.MODE_PRIVATE)
+
+        val salesPersonName = sharedPreferences.getString(getString(R.string.profile_name_key),getString(R.string.profile_name_key_default))
+
+        paint.textAlign =LEFT
+
+        canvas.drawText("Salesperson Name: $salesPersonName",inchFloat,inchFloat * 2 + halfInchFloat,paint)
+
+        canvas.drawText("Client: ${client.name}",inchFloat,inchFloat * 3 + halfInchFloat,paint)
+
+        canvas.drawText("Order: ${client.order}",inchFloat,inchFloat * 4 + halfInchFloat,paint)
+
+        canvas.drawText("Terms: ${client.terms}",inchFloat,inchFloat * 5 + halfInchFloat,paint)
+
+        document.finishPage(page)
+
+        val file = File(directory,FILE_NAME)
+
+        try{
+            document.writeTo(FileOutputStream(file))
+        }catch (e : IOException){
+            Log.e("TAG", "buildPdf: ${e.toString()}",e )
+        }
+
+        document.close()
+
+
+
+    }
+
+    fun Context.proposalExists(id : Int): Boolean {
+        return File(this.getFolder(id),FILE_NAME).exists()
+    }
+
+    fun Context.getFiles(id:Int): List<File> {
+        return this.getFolder(id).listFiles().asList()
+    }
--- app/src/main/java/com/psdemo/globomanticssales/ui/profile/ProfileFragment.kt
+++ app/src/main/java/com/psdemo/globomanticssales/ui/profile/ProfileFragment.kt
@@ -1,11 +1,16 @@
 package com.psdemo.globomanticssales.ui.profile
 
+import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.google.android.material.snackbar.Snackbar
+import com.psdemo.globomanticssales.MainActivity
 import com.psdemo.globomanticssales.R
+import kotlinx.android.synthetic.main.fragment_profile.*
+import kotlinx.android.synthetic.main.fragment_profile.txtName
 
 class ProfileFragment : Fragment() {
     override fun onCreateView(
@@ -14,5 +19,25 @@ class ProfileFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.fragment_profile, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        val sharedPref = requireActivity().getSharedPreferences(getString(R.string.profile_preference_key),
+            Context.MODE_PRIVATE)
+
+        txtName.setText(
+            sharedPref.getString(getString(R.string.profile_name_key),getString(R.string.profile_name_key_default))
+        )
+
+        btnSave.setOnClickListener{
+            with(sharedPref.edit()){
+                putString(getString(R.string.profile_name_key),txtName.text.toString())
+                apply()
+            }
+
+            (activity as MainActivity).hideKeyboard()
+            Snackbar.make(it,"Changes succesfully saved",Snackbar.LENGTH_LONG).show()
+
+        }
     }
 }
